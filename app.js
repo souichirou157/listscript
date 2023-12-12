3b675e92351847c6d0ac735b9d4eddb0a95820e3
tarou <tarou7684@gmail.com>
2023-12-12 21:57:39 +0900

    move color code from cascard-stylesheet

--- app.js
+++ app.js
@@ -3,19 +3,46 @@ const button = document.getElementById('add');
 const text = document.getElementById('param');
 const del = document.getElementById('del');
 const ul = document.createElement('ul');
-document.querySelector('body').appendChild(ul);
 const checkbox = document.getElementById('checkbox');
-ul.classList.add('ul');
-let i=0;
+const rem_error =document.createElement('p');
+const link =document.querySelector('#save');
+const alldel =document.querySelector('#alldel');
+const error = document.querySelector('#error');
 const list=[];
+
+rem_error.id = 'ul.length_zeros';
+error.appendChild(rem_error);
+ul.classList.add('ul');
+document.querySelector('body').appendChild(ul);
+let i=0;
 let res_num =1;
+
+////////////// cascard  style /////////////////////
+ 
+alldel.textContent = '一括削除を有効にする';
+button.textContent = '登録';
+del.textContent = '削除';
+link.textContent = 'データを保存する';
+del.style.cursor = 'pointer';
+link.style.cursor = 'pointer';
+link.classList.add('save');
+/*-------------------------style color ----------------------------------*/
+button.style.background = 'var(--color)';
+document.querySelector('.time').style.color = 'var(--time)';
+document.querySelector('body').style.background= ' var(--body)'; // 
+alldel.style.color = 'var(--del)';
+del.style.color = 'var(--del)';
+link.style.color = 'rgb(230,82,90)';
+error.style.background = 'var(--text)';
+link.style.background = 'var(--save)';
+/*---------------------------------------------------------*/
+
 
 //-------------methodes-----------------//
 function count(){
     setTimeout(clock(),1000);
  
 }
-
 
 function clock(){
     const now = new Date(); 
@@ -29,11 +56,9 @@ function clock(){
     li.textContent = hh+':' + mm + ':' + ss;
     list.push(li);
     document.querySelector('ul').appendChild(list[i]);
-    document.querySelector('.log').appendChild(ul);
+    document.querySelector('.time').appendChild(ul);
     i++;
 }
-
-
 
 
 function addList(){
@@ -46,32 +71,33 @@ function addList(){
         list.push(li);
         ul.id = 'list';
         document.querySelector('ul').appendChild(list[i]);
-        document.querySelector('.log').appendChild(ul);
+        document.querySelector('.time').appendChild(ul);
         i++;
 }
 
 
 //古い奴からけしていく(（笑）)
 
-//ほかに良い方法思いつくまでとりあえずこれで　
+//ほかに良い方法思いつくまでとりあえずこれで
 function Delete(){
     for(let i=0 ; i < 2;i++){
         ul.firstChild.remove(ul.firstChild);
     }
- 
+    if(!ul.firstChild){
+     rem_error.textContent ='現在データはありません';  
+         
+    } 
 }
+
 
 //リスト全削除
 function AllDelete(){
     while(ul.firstChild){ // list exit first child
         ul.removeChild(ul.firstChild); //delete
     }
+    rem_error.textContent ='現在データはありません';  
+         
 }
-
-
-
-
-
 
 
 
@@ -89,7 +115,7 @@ function objectsJounal() {
     }
     console.log(charArray.length);
     console.log(spaceArray.length);
-    if(charArray.length < spaceArray.length || charArray.length === 0){
+    if(charArray.length <= spaceArray.length || charArray.length === 0){
         button.disabled = true;
         window.alert('空白文字数が不正です');
     }else {
@@ -117,10 +143,12 @@ button.addEventListener('click',()=>{
     objectsJounal();
     text.value = '';
     button.disabled =true;
+    rem_error.textContent ='';
 
 });
   
 
+//レス番号リセットはオールデリートだけ
     //--delete action
 del.addEventListener('click',()=>{
  
@@ -142,12 +170,35 @@ del.addEventListener('click',()=>{
             console.log(ul.children);
         }
 
-
     });
 
+
+
+
+    
+    
+
+
+
+
        
-        
-   
+/*ここまだうまくいってない*/
+function saveText(){
+    //何とかしてtextcontentしたやつを持たせたい
+    const fi= new Blob(ul.children.textContent,{type:"text/javascript"});
+    link.href = URL.createObjectURL(fi);
+    const logfile = fi.CreateTextFile("log.txt");    
+    link.download = 'log.txt';
+
+}
+
+
+ 
+
+
+link.addEventListener('click',()=>{
+    saveText();
+});
 
 
 
